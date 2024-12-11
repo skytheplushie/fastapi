19fd01562685f5858ee0c7ffa0f51b53b1cc6e80
skytheplushie <ole16307@gmail.com>
2024-12-11 14:43:56 +0500

    Add files via upload

--- adigeli we.py
+++ adigeli we.py
@@ -1,32 +1,45 @@
-from fastapi import FastAPI, Path
-from typing import Annotated
+from fastapi import FastAPI, Path, HTTPException
+from typing import Annotated, List
+from pydantic import BaseModel
 
-app = FastAPI(swagger_ui_parameters=({"TryItOut Enabled":True}))
+app = FastAPI(swagger_ui_parameters=({"TryItOut Enabled": True}))
 
-users = {'1': 'Имя: Example, возраст: 18'}
+# users = {'1': 'Имя: Example, возраст: 18'}
+users = []
+
+
+class User(BaseModel):
+    id: int
+    username: str
+    age: int
 
 
 @app.get('/users')
-async def get_users():
+async def get_users() -> List[User]:
     return users
 
 
 @app.post('/user/{username}/{age}')
 async def add_user(username: Annotated[str, Path(min_length=5, max_length=20,
-                                                      description='Enter username', example='UrbanUser')],
-                        age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='24')]):
-    user_id = str(int(max(users, key=int)) + 1)
-    users.update({user_id: f'Имя: {username}, возраст: {age}'})
-    return f"User {user_id} is registered"
+                                                 description='Enter username', example='UrbanUser')],
+                   age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='24')]) -> User:
+    user_id = max(users, key=lambda x: int(x.id)).id + 1 if users else 1
+    user = User(id=user_id, username=username, age=age)
+    users.append(user)
+    return user
 
 
 @app.put('/user/{user_id}/{username}/{age}')
 async def update_user(user_id: Annotated[int, Path(ge=1, le=100, description='Enter User ID', example='1')],
-                        username: Annotated[str, Path(min_length=5, max_length=20,
-                                                      description='Enter username', example='UrbanUser')],
-                        age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='24')]):
-    users[user_id] = f'Имя: {username}, возраст: {age}'
-    return f"The user {user_id} is updated"
+                      username: Annotated[str, Path(min_length=5, max_length=20,
+                                                    description='Enter username', example='UrbanUser')],
+                      age: Annotated[int, Path(ge=18, le=120, description='Enter age', example='24')]) -> User:
+    for user in users:
+        if user_id == user_id:
+            user.username = username
+            user.age = age
+            return user
+    raise HTTPException(status_code=404, detail="User was not found")
 
 
 @app.delete('/user/{user_id}')
