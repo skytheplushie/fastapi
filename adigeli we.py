b3a62b96bcdaa18a8fef2897ae72ddd4c5b36759
skytheplushie <ole16307@gmail.com>
2024-12-12 13:02:27 +0500

    Add files via upload

--- adigeli we.py
+++ adigeli we.py
@@ -1,8 +1,11 @@
-from fastapi import FastAPI, Path, HTTPException
+from fastapi import FastAPI, Path, HTTPException, Request
 from typing import Annotated, List
 from pydantic import BaseModel
+from fastapi.templating import Jinja2Templates
+from fastapi.responses import HTMLResponse
 
 app = FastAPI(swagger_ui_parameters=({"TryItOut Enabled": True}))
+templates = Jinja2Templates
 
 # users = {'1': 'Имя: Example, возраст: 18'}
 users = []
@@ -12,6 +15,20 @@ class User(BaseModel):
     id: int
     username: str
     age: int
+
+
+@app.get("/")
+async def root(request: Request) -> HTMLResponse:
+    return templates.TemplateResponse('users.html', {'request': request, 'user': user})
+
+
+@app.get("/user/{user_id}")
+async def get_user_page(request: Request,
+                        user_id: Annotated[int, Path(ge=1, le=100, description="Enter User ID", example=1)]) -> HTMLResponse:
+    for user in users:
+        if int(user_id) == user_id:
+            return templates.TemplateResponse('user.html', {'request': request, 'user': user})
+    raise HTTPException(status_code=404, detail='User was not found')
 
 
 @app.get('/users')
